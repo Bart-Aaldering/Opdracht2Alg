d03c33330db3f7a9a2304f0792eed790f4002bf0
Bart-Aaldering <79908741+Bart-Aaldering@users.noreply.github.com>
2021-04-13 15:13:08 +0200

    Update rooster.cc

--- rooster.cc
+++ rooster.cc
@@ -4,6 +4,7 @@
 #include <fstream>
 #include "standaard.h"
 #include "rooster.h"
+#include <cstring>
 using namespace std;
 
 //*************************************************************************
@@ -19,8 +20,63 @@ Rooster::Rooster ()
 
 bool Rooster::leesIn (const char* invoerNaam)
 {
-	// TODO: implementeer deze memberfunctie
+	ifstream invoer (invoerNaam, ios::in);
 
+	if (!invoer.is_open()) {
+		cout << "Kan file niet openen." << endl;
+		return false;
+	}
+
+	invoer >> nrDagen;
+	invoer >> nrUrenPerDag;
+	invoer >> nrZalen;
+	invoer.get();
+
+	invoer >> nrDocenten;
+	invoer.get();
+
+	for(int i = 0; i < nrDocenten; i++) {
+		docenten[i] = new Docent;
+
+		invoer >> docenten[i]->nrBeschikbareTijdsloten;
+		invoer.get();
+
+		for(int j = 0; j < docenten[i]->nrBeschikbareTijdsloten; j++) {
+			invoer >> docenten[i]->beschikbareTijdsloten[j];
+		}
+		invoer.get();
+	}
+	invoer.get();
+
+	invoer >> nrVakken;
+	invoer.get();
+
+	for (int i = 0; i < nrVakken; i++) {
+		vakken[i] = new Vak;
+
+		invoer >> vakken[i]->naam;
+		invoer.get();
+
+		invoer >> vakken[i]->docent;
+		invoer >> vakken[i]->nrTracks;
+		invoer.get();
+
+		for(int j = 0; j < vakken[i]->nrTracks; j++) {
+			invoer >> vakken[i]->tracks;
+		}
+		invoer.get();
+	}
+	for (int i = 0; i < nrVakken; i++) {
+		cout << vakken[i]->naam << endl;
+
+		cout << vakken[i]->docent;
+		cout << vakken[i]->nrTracks << endl;
+
+		for(int j = 0; j < vakken[i]->nrTracks; j++) {
+			cout << vakken[i]->tracks << ' ';
+		}
+		cout << endl;
+	}
 	return true;
 }  // leesIn
 
