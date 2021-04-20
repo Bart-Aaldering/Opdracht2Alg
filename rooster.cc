f292927c12e3e7b909db9075653b8169946696da
LexJanssens <s2989344@vuw.leidenuniv.nl>
2021-04-20 15:35:46 +0200

    Update rooster.cc

--- rooster.cc
+++ rooster.cc
@@ -26,7 +26,9 @@ bool Rooster::leesIn (const char* invoerNaam)
 		cout << "Kan file niet openen." << endl;
 		return false;
 	}
-
+	
+	invoernaam = invoerNaam; //slaat invoernaam op 
+	
 	invoer >> nrDagen;
 	invoer >> nrUrenPerDag;
 	invoer >> nrZalen;
@@ -82,7 +84,40 @@ bool Rooster::leesIn (const char* invoerNaam)
 
 void Rooster::drukAf ()
 {
-	// TODO: implementeer deze memberfunctie
+	cout << "---------- Info " << invoernaam << " ----------" << endl;
+	cout << "Dagen: " << nrDagen << endl;
+	cout << "Uren per dag: " << nrUrenPerDag << endl;
+	cout << "Hoeveelheid zalen: " << nrZalen << endl;
+	cout << "Hoeveelheid docenten: " << nrDocenten << endl;
+	cout << "Tijdschema's docenten:" << endl;
+	for (int i = 0; i < nrDocenten; i++) {
+		cout << "  Docent: " << i << endl;
+		cout << "  Hoeveelheid beschikbare tijdsloten: " << docenten[i]->nrBeschikbareTijdsloten << endl;
+		cout << "  Tijdsloten: ";
+		for (int j = 0; j < docenten[i]->nrBeschikbareTijdsloten -1; j++) {
+			cout << docenten[i]->beschikbareTijdsloten[j] << ",";
+		}
+		cout << docenten[i]->beschikbareTijdsloten[docenten[i]->nrBeschikbareTijdsloten-1] << endl;
+		cout << endl;
+	}
+	cout << "Hoeveelheid vakken: " << nrVakken << endl;
+	cout << "Vak info:" << endl;
+	for (int i = 0; i < nrVakken; i++) {
+		cout << "  Naam vak: " << vakken[i]->naam << endl;
+		cout << "  Docent: " << vakken[i]->docent << endl;
+		cout << "  Hoeveelheid tracks: " << vakken[i]->nrTracks << endl;
+		cout << "  Deelnemende tracks: ";
+
+		for (int j = 0; j < vakken[i]->nrTracks-1; j++) {
+			cout << vakken[i]->tracks[j] << ", ";
+		}
+		cout << vakken[i]->tracks[vakken[i]->nrTracks-1] << endl;
+		cout << endl;
+	}
+	for (int i = 0; i < 27 + invoernaam.length(); i++) {
+		cout << "-";
+	}
+	cout << endl << endl;
 
 }  // drukAf ()
 
@@ -110,8 +145,57 @@ bool Rooster::bepaalMinRooster (int rooster[MaxNrTijdsloten][MaxNrZalen],
 
 void Rooster::drukAfRooster (int rooster[MaxNrTijdsloten][MaxNrZalen])
 {
-	// TODO: implementeer deze memberfunctie
+	for (int i = 1; i < nrDagen+1; i++) {
+		cout << " Dag " << i;
+		for (int l = 0; l < 26-((i/10)+1); l++) { //compenseer met spaties
+			cout << " ";
+		}
+	}
+	cout << endl;
+	for (int j = 0; j < nrUrenPerDag; j++) {
+		for (int i = 0; i < nrDagen; i++) {
+			cout << "+------------------------------"; //30 streepjes
+		}
+		cout << "+" << endl;
+		for (int k = 0; k < nrDagen; k++) {
+			cout << "| " << vakken[0]->naam; //vak
+			for (int l = 0; l < 29-vakken[0]->naam.length(); l++) { //compenseer met spaties
+				cout << " ";
+			}
+		}
 
+		cout << "| Uur" << endl;
+		for (int k = 0; k < nrDagen; k++) {
+			cout << "| Docent: " << vakken[0]->docent; //docent
+			for (int l = 0; l < 20; l++) { //compenseer met spaties
+				cout << " ";
+			}
+		}
+		cout << "| " << j + 1 << endl;
+		for (int k = 0; k < nrDagen; k++) {
+			cout << "| Tracks: ";
+			cout << vakken[0]->tracks[0]; //print de eerste alvast, voor de komma's
+			for (int l = 1; l < vakken[0]->nrTracks; l++)
+				cout << ", " << vakken[0]->tracks[l]; //track
+			for (int l = 0; l < 20 - ((vakken[0]->nrTracks-1)*3); l++) { //compenseer met spaties
+				cout << " ";
+			}
+		}
+		cout << "|" << endl;
+		for (int k = 0; k < nrDagen; k++) {
+			cout << "| Zaal: " << nrZalen; //zaal
+			for (int l = 0; l < 22; l++) {
+				cout << " "; //compenseer met spaties
+			}
+		}
+		cout << "|" << endl;
+
+	}
+
+	for (int i = 0; i < nrDagen; i++) {
+		cout << "+------------------------------"; //30 streepjes
+	}
+	cout << "+" << endl;
 }  // drukAfRooster
 
 //*************************************************************************
